69b9d17031cfaa7c10b46796c37e09b83d769efc
Sanya <sanyajarial@Sanyas-MacBook-Air.local>
2026-08-29 14:31:41 -0400

    Module 2 hw

--- Module2/student_manager.py
+++ Module2/student_manager.py
@@ -37,4 +37,37 @@ def find_by_name_case_insensitive(name):
     return False
 print(find_by_name_case_insensitive("ASHA"))
 
-# %% Binary Search - Task 4 Binary Search Dry run on paper
+# %% Binary Search - Task 4 + 5 Binary Search Dry run
+values = [3,7,11,18,24,31,42,56,70]
+target = 42
+
+def binary_search(values, target):              #best case: mid == target O(1)
+    left = 0                                    #worst case: divide, conquer O(log n)
+    n = len(values)
+    right = n-1
+    while left <= right:
+        mid = (left+right)//2
+        if values[mid] == target:
+            return mid          #index returned
+        if values[mid] < target:
+            left = mid + 1
+        elif values[mid] > target:
+            right = mid - 1
+    return -1
+print(binary_search(values, target))
+
+
+# %% For values [2,5,8,12,16,23,38,56,72,91], search 23.
+#               left    right   mid     values[mid]
+#                0      9       4       16
+#                5      9       7       56
+#                5      6       5       23
+
+#search 40
+#               left    right   mid     values[mid]
+#                0      9       4       16
+#                5      9       7       56
+#                5      6       5       23
+#                6      6       6       38
+#                7      6       -       STOP  
+
